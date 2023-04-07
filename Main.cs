08e89954bec0e6f6776547697e0bd25c0cf79726
Kwispy <dementievdmitriy.d@gmail.com>
2023-04-07 17:59:32 +0500

    Add Main.cs

---
+++ Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test1
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
